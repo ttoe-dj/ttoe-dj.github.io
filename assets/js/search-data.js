a74d5b2007cf31cb03d71b9e4c7b25736d33c209
ttoe-dj <djia.research@gmail.com>
2025-10-30 20:09:47 +0000

    Deploying to gh-pages from @ ttoe-dj/ttoe-dj.github.io@40f13049c766d6e88c3f82bb3dec69f3e733de05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,15 +44,15 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-",
+        },{id: "post-notes-on-lockwood-1996-quot-many-minds-interpretations-of-quantum-mechanics",
         
-          title: "",
+          title: "Notes on Lockwood (1996) &quot;Many Minds Interpretations of Quantum Mechanics”",
         
         description: "",
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/posts/2025/2025-10-30-Lockwood1996/";
+            window.location.href = "/posts/2025/Lockwood1996/";
           
         },
       },{id: "post-notes-on-albert-amp-loewer-1988-interpreting-the-many-worlds-interpretation",
