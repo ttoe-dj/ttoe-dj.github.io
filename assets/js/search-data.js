9661561e231a116f8e5bf7e6c50c5fb49fee5996
ttoe-dj <djia.research@gmail.com>
2025-10-30 19:21:04 +0000

    Deploying to gh-pages from @ ttoe-dj/ttoe-dj.github.io@bbfc8016bfd2ca027df3917bae11c695a155aeac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-notes-on-albert-amp-loewer-1988-interpreting-the-many-worlds-interpretation",
+        },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/posts/2025/2025-10-30-Lockwood1996/";
+          
+        },
+      },{id: "post-notes-on-albert-amp-loewer-1988-interpreting-the-many-worlds-interpretation",
         
           title: "Notes on Albert &amp; Loewer (1988) “Interpreting the many worlds interpretation”",
         
